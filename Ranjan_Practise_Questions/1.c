d5d33b2723610e254818da81b213e28829168cc5
Surya <suryask7549@gmail.com>
2024-12-28 15:17:35 +0530

    Files Rearranged

---
+++ Ranjan_Practise_Questions/1.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+int main()
+{
+    // int num = 167;
+
+    // // Condition Check Starts
+    // if (num == 10)
+    // {
+    //     printf("ten");
+    // }
+    // else if (num == 45)
+    // {
+    //     printf("45");
+    // }
+    // else if (num == 60)
+    // {
+    //     printf("60");
+    // }
+    // else
+    // {
+    //     printf("other");
+    // }
+
+    // ******** ARRAYS ***********
+
+    // int arr[]={10,20,30,50};
+
+    // int arr[4];
+    // arr[4] = {1, 2, 3, 4};
+
+    // int arr[4];
+    // arr[0] = 1;
+    // arr[1] = 2;
+    // arr[2] = 3;
+    // arr[3] = 4;
+
+    // User Input Array
+    int arr[4];
+    printf("Enter four elements");
+    for(int i=0;i<4;i++)
+    { 
+        scanf("%d");
+    }
+
+    return 0;
+}
